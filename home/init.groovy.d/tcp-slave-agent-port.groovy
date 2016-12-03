4828fced3b10e3b1f3e79689a14e9fc9cb97025d
jnbnyc <jnbuhaynyc@gmail.com>
2016-12-03 16:34:59 -0500

    Update files in init.groovy.d

--- home/init.groovy.d/tcp-slave-agent-port.groovy
+++ home/init.groovy.d/tcp-slave-agent-port.groovy
@@ -1,12 +1,22 @@
+@Grapes([
+    @Grab(group='org.yaml', module='snakeyaml', version='1.17')
+])
+
+import org.yaml.snakeyaml.Yaml
+import java.util.logging.Logger
+
 import hudson.model.*;
 import jenkins.model.*;
 
+JENKINS_SETUP_YAML = env['JENKINS_SETUP_YAML'] ?: "${env['JENKINS_CONFIG_HOME']}/setup.yml"
+Logger logger = Logger.getLogger('tcp-slave-agent-port.groovy')
+def config = new Yaml().load(new File(JENKINS_SETUP_YAML).text)
 
 Thread.start {
-      sleep 10000
-      println "--> setting agent port for jnlp"
-      def env = System.getenv()
-      int port = env['JENKINS_SLAVE_AGENT_PORT'].toInteger()
-      Jenkins.instance.setSlaveAgentPort(port)
-      println "--> setting agent port for jnlp... done"
+    sleep 8000
+    logger.info('--> setting agent port for jnlp')
+    def env = System.getenv()
+    int port = config.jnlp_port ?: env['JENKINS_SLAVE_AGENT_PORT'].toInteger() ?: 5001
+    Jenkins.instance.setSlaveAgentPort(port)
+    logger.info('--> setting agent port for jnlp... done')
 }
