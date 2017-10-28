c59bf6db613fc546139e57e6e8895afcb5914ef1
jnbnyc <jnbuhaynyc@gmail.com>
2017-10-28 07:36:54 -0400

    Refactor tcp-slave-agent-port.groovy

--- jenkins_home/init.groovy.d/tcp-slave-agent-port.groovy
+++ jenkins_home/init.groovy.d/tcp-slave-agent-port.groovy
@@ -1,22 +1,34 @@
-@Grapes([
-    @Grab(group='org.yaml', module='snakeyaml', version='1.17')
-])
+@Grab(group='org.yaml', module='snakeyaml', version='1.18')
 
-import org.yaml.snakeyaml.Yaml
+import groovy.transform.Field
 import java.util.logging.Logger
+import jenkins.model.Jenkins
+import org.yaml.snakeyaml.Yaml
 
-import hudson.model.*;
-import jenkins.model.*;
-
-JENKINS_SETUP_YAML = env['JENKINS_SETUP_YAML'] ?: "${env['JENKINS_CONFIG_HOME']}/setup.yml"
+@Field
 Logger logger = Logger.getLogger('tcp-slave-agent-port.groovy')
-def config = new Yaml().load(new File(JENKINS_SETUP_YAML).text)
+
+@Field
+def env = System.getenv()
+
+@Field
+def JENKINS_SETUP_YAML = env['JENKINS_SETUP_YAML'] ?: "${env['JENKINS_CONFIG_HOME']}/setup/main.yml"
+
+@Field
+def config = null
+
+try {
+  config = new Yaml().load(new File(JENKINS_SETUP_YAML).text)
+} catch (java.io.FileNotFoundException f) {
+  logger.warning("--> No configuration file found at ${JENKINS_SETUP_YAML}")
+  return
+}
+
 
 Thread.start {
-    sleep 8000
+    sleep(8000)
     logger.info('--> setting agent port for jnlp')
-    def env = System.getenv()
-    int port = config.jnlp_port ?: env['JENKINS_SLAVE_AGENT_PORT'].toInteger() ?: 5001
+    int port = config.jnlp_port ?: env['JENKINS_SLAVE_AGENT_PORT'].toInteger() ?: 50000
     Jenkins.instance.setSlaveAgentPort(port)
     logger.info('--> setting agent port for jnlp... done')
 }
